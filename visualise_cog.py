ec477da4d7f52823f7b154f0b25da3dafd4e7f2d
EngineerCoding <EngineerCoding@hotmail.nl>
2017-03-27 00:28:09 +0100

    Implement configurations for the output

--- visualise_cog.py
+++ visualise_cog.py
@@ -1,45 +1,44 @@
-from sys import argv, exit
+from PIL import Image, ImageDraw, ImageFont
+from argparse import ArgumentParser
 from psycopg2 import connect
+from sys import argv, exit
 import math
-try:
-    from PIL import Image, ImageDraw, ImageFont
-except ImportError:
-    from sys import executable
-    from os import system
-    if (input("Do you want to install the pillow dependency? (y/n)")
-            .strip().lower() == "y"):
-        system(executable + " -m pip install pillow")
-        print("Restart this script")
-        exit()
-    print("pillow dependency not found")
-    exit(-1)
-
-
-def to_number(argument_name, value):
-    if value.strip().isnumeric():
-        return int(value.strip())
-    print("Invalid argument for {} with \"{}\"".format(argument_name, value))
-    exit()
-
-
-def expected_cog_number():
-    print("Expected cog number")
-    exit()
 
 
 def handle_program_arguments():
-    del argv[0]
-    if len(argv) == 0:
-        expected_cog_number()
-    cog_id = to_number("COG", argv[-1])
-    canvas_size = 500
-    if "--size" in argv:
-        index = argv.index("--size")
-        if index == len(argv) - 1:
-            expected_cog_number()
-        if index + 1 < len(argv):
-            canvas_size = to_number("--size", argv[index + 1])
-    return abs(cog_id), abs(canvas_size)
+    parser = ArgumentParser()
+    t = lambda n: abs(int(n))
+    w = (0, 0, 0)
+    parser.add_argument("--canvas-size", "-cs", default=500, type=t,
+                        help="Amount of pixels the square canvas should be")
+    parser.add_argument(dest="cog_id", type=t, help="The cog ID in the data" +
+                        "base to create the image for")
+    parser.add_argument("--font-size", "-fs", default=11, type=t,
+                        help="The size of the font the la" +
+                        "bels are written with")
+    parser.add_argument("--line-width", "-lw", default=2, type=t,
+                        help="The width of the lines")
+    parser.add_argument("--background-color", "-bc", type=t, nargs=3,
+                        default=(255, 255, 255), choices=range(256),
+                        help="The color of the background", metavar='0-255')
+    parser.add_argument("--label-color", "-lc", default=w, choices=range(256),
+                        help="The color of the label text", type=t,
+                        metavar='0-255')
+    parser.add_argument("--hit-color", "-hc", default=w, type=t, nargs=3,
+                        choices=range(256), metavar='0-255',
+                        help="The color of a line for a existing bidirectio" +
+                        "nal hit")
+    parser.add_argument("--theoretical-color", "-tc", default=(255, 0, 0),
+                        type=t, nargs=3, help="The color of a line for a no" +
+                        "t existing bidirectional hit", choices=range(256),
+                        metavar='0-255')
+    parser.add_argument("--output-file", "-o",
+                        help="The file to save the image in")
+    options = parser.parse_args()
+    for attr in ("background_color", "label_color", "hit_color",
+                 "theoretical_color"):
+        setattr(options, attr, tuple(getattr(options, attr)))
+    return options
 
 
 def get_cog_data(cog_id):
@@ -58,7 +57,7 @@ def get_cog_data(cog_id):
     return cog_proteins, cog_protein_hits
 
 
-def draw_label(draw, point, radians, label, font):
+def draw_label(opts, draw, point, radians, label, font):
     if radians < 0.5 * math.pi or radians > 1.5 * math.pi:
         point[0] += 5
     elif radians > 0.5 * math.pi or radians < 1.5 * math.pi:
@@ -66,12 +65,12 @@ def draw_label(draw, point, radians, label, font):
     if radians < math.pi:
         point[1] -= 10
     elif math.pi < radians < 2 * math.pi:
-        point[1] += 10
-    draw.text(tuple(point), label, fill=(0, 0, 0))
+        point[1] += 10 - opts.font_size
+    draw.text(tuple(point), label, fill=opts.label_color, font=font)
 
 
-def get_and_draw_protein_points(draw, proteins, radius, canvas_size, font):
-    center_point = math.floor(canvas_size / 2)
+def get_and_draw_protein_points(opts, draw, proteins, radius, font):
+    center_point = math.floor(opts.canvas_size / 2)
     point_map = dict()
     circle_step = 2 / len(proteins)
     for i in range(0, len(proteins)):
@@ -79,46 +78,45 @@ def get_and_draw_protein_points(draw, proteins, radius, canvas_size, font):
         x = math.floor(abs(radius * math.cos(radians) + center_point) + 0.5)
         y = math.floor(abs(radius * math.sin(radians) - center_point) + 0.5)
         point_map[proteins[i]] = (x, y)
-        draw_label(draw, [x, y], radians, str(proteins[i]), font)
+        draw_label(opts, draw, [x, y], radians, str(proteins[i]), font)
     return point_map
 
 
 def draw_connections(draw, real_hits, point_map, theoretic_hits=[],
-                     color=(0, 0, 0)):
+                     color=(0, 0, 0), line_width=2):
     for protein_a, protein_b in real_hits:
         point_list = [point_map[protein_a], point_map[protein_b]]
-        draw.line(point_list, fill=color, width=2)
+        draw.line(point_list, fill=color, width=line_width)
         for hit in [(protein_a, protein_b), (protein_b, protein_a)]:
             if hit in theoretic_hits:
                 theoretic_hits.remove(hit)
 
 
-def generate_cog_visualisation(proteins, protein_hits, canvas_size,
-                               output_file):
-    image = Image.new("RGB", (canvas_size, canvas_size), color=(255, 255,
-                                                                255))
-    font = ImageFont.load_default()
+def generate_cog_visualisation(opts, proteins, protein_hits):
+    image = Image.new("RGB", (opts.canvas_size,) * 2,
+                      color=opts.background_color)
+    font = ImageFont.truetype("Vera.ttf", size=opts.font_size)
     text_width = 2 * max(map(lambda n: font.getsize(str(n))[0], proteins))
     drawing_instance = ImageDraw.Draw(image)
     protein_point_map = get_and_draw_protein_points(
-        drawing_instance, proteins, (canvas_size - 10 - text_width) / 2,
-        canvas_size, font)
+        opts, drawing_instance, proteins,
+        (opts.canvas_size - opts.font_size - text_width) / 2, font)
     all_hits = []
     for index in range(len(proteins)):
         for combine_with in proteins[index + 1:]:
             all_hits.append((proteins[index], combine_with))
     draw_connections(drawing_instance, protein_hits, protein_point_map,
-                     theoretic_hits=all_hits)
+                     theoretic_hits=all_hits, color=opts.hit_color,
+                     line_width=opts.line_width)
     draw_connections(drawing_instance, all_hits, protein_point_map,
-                     color=(255, 0, 0))
-    image.save(output_file, "PNG")
+                     color=opts.theoretical_color, line_width=opts.line_width)
+    image.save(opts.output_file or "{}.png".format(opts.cog_id), "PNG")
 
 
 def main():
-    cog_id, canvas_size = handle_program_arguments()
-    cog_proteins, cog_protein_hits = get_cog_data(cog_id)
-    generate_cog_visualisation(cog_proteins, cog_protein_hits, canvas_size,
-                               str(cog_id) + ".png")
+    options = handle_program_arguments()
+    cog_proteins, cog_protein_hits = get_cog_data(options.cog_id)
+    generate_cog_visualisation(options, cog_proteins, cog_protein_hits)
 
 
 main()
